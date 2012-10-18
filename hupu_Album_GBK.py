0742b97040fc20beeb272881d47c1318d622b473
xavier skip <xavierskip@gmail.com>
2012-10-18 20:00:27 +0800

    v 0.15

--- hupu_Album_GBK.py
+++ hupu_Album_GBK.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python
 #coding: GBK
 #author: xavierskip
-#version: 0.13
+#version: 0.15
 #date: 10-18-2012
 
 import os,sys,urllib2,re
 
-def get_pages(home,homepage):
-	match = re.match(r'http://my\.hupu\.com([\S]+?)(?:\-[\d]\.html|\.html)',home) 
-	if match == None:
-		print 'some thing wrong!\n说好了不要拿乱七八糟的url来调戏我的！'
-		exit()
-	path = match.group(1)    #Album path
-	pat  = path+ r'-([\d]+).html(?:\'|\")>'
+def get_pages(home,path):
+	homepage = get_content(home)    #字符编码真烦人
+	title    = re.search(r'<title>(.+)</title>',homepage).group(1)
+	print '正在抓取相册>>>%s>>>' %title
+	pat  = path+ r'-([\d]+).html(?:\'|\")>'       #匹配剩下的相册页面
 	P_nums = re.findall(pat,homepage)
 	if P_nums==[]:
 		page_list = [home]
-		return page_list
+		return page_list,title
 	else:
 		P_max = int(P_nums[-1])
 		#生成所有相册页面，还好都是有规律的。汗|||
 		page_list = [r'http://my.hupu.com%s-%d.html' %(path,i) for i in xrange(1,P_max+1)]
-		return page_list
-
-def get_content(url):
-	content = urllib2.urlopen(url).read()
-	return content
+		return page_list,title
 
 def get_urls(content):
 	pat = r'http://i[\d]{1}\.hoopchina\.com\.cn/.+small\.(?:jpg|gif|png|jpeg|bmp)'
@@ -42,6 +36,9 @@ def get_urls(content):
 	urls = re.sub(r'small\.',r'big.',urls)
 	return urls
 
+def get_content(url):
+	return urllib2.urlopen(url).read()
+
 def dowm_img(urls):
 	pass
 
@@ -51,10 +48,14 @@ def main():
 		home = r'http://my.hupu.com/jackson817/photo/a82914-1.html'    #脚本内置虎扑相册地址
 	else:
 		home = sys.argv[1]
-	homepage = urllib2.urlopen(home).read()    #字符编码真烦人
-	title    = re.search(r'<title>(.+)</title>',homepage).group(1)
-	print '正在抓取相册>>>%s>>>' %title
-	page_list = get_pages(home,homepage)    #得到相册的所有页面
+	# 判断url是否正确
+	match = re.match(r'http://my\.hupu\.com(/[\S]+?/photo/a[\d]+?)(?:\-[\d]+\.html|\.html)',home) 
+	if match == None:
+		print 'some thing wrong!\n说好了不要拿乱七八糟的url来调戏我的！'
+		exit()
+	path = match.group(1)    #Album path
+	print '\n','='*10,'开始抓取','='*10,'\n'
+	page_list,title = get_pages(home,path)    #得到相册的所有页面
 	P_num =  len(page_list)
 	print '此相册有%d页！' %P_num
 	content = ''
@@ -77,7 +78,7 @@ def main():
 	if end == 1:
 	    print '缺少wget！可以打开"%s"文件夹下的urls文件，复制其内容用其他下载工具下载，例如迅雷等~' %title
 	else:
-	    print 'success!'
+	    print 'well done!'
 		
 if __name__ == '__main__':
     main()
